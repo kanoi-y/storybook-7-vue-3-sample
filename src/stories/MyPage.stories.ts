c2f8b7ea2e5fd3fa92eb7ab96eb91e626ea184f8
kanoi-y <furafuratonbo@gmail.com>
2023-05-14 09:41:32 +0900

    ✨ MyPageのストーリーをviewportごとに作成

--- src/stories/MyPage.stories.ts
+++ src/stories/MyPage.stories.ts
@@ -10,8 +10,43 @@ const meta: Meta<typeof MyPage> = {
     components: { MyPage },
     template: "<MyPage />",
   }),
+  // PC, Mobile それぞれのビューポートを作成 (高さは指定しない)
+  parameters: {
+    viewport: {
+      viewports: {
+        pc: {
+          name: "Min PC Layout",
+          styles: {
+            width: "992px",
+            height: "100%",
+          },
+        },
+        mobile: {
+          name: "Min Mobile Layout",
+          styles: {
+            width: "375px",
+            height: "100%",
+          },
+        },
+      },
+    },
+  },
 };
 
-export const Default: Story = {};
+export const ForPc: Story = {
+  parameters: {
+    viewport: {
+      defaultViewport: "pc",
+    },
+  },
+};
+
+export const ForMobile: Story = {
+  parameters: {
+    viewport: {
+      defaultViewport: "mobile",
+    },
+  },
+};
 
 export default meta;
